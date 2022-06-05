5cd56a527743b4910cbf58272c5030babd0497fd
mikseros <mmadynski@gmail.com>
2022-06-05 14:45:48 +0200

    Info logs for Service

--- src/main/java/com/mikseros/userservice/service/UserServiceImpl.java
+++ src/main/java/com/mikseros/userservice/service/UserServiceImpl.java
@@ -24,16 +24,19 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public User saveUser(User user) {
+		log.info("Saving new user {} to the database", user.getName());
 		return userRepo.save(user);
 	}
 
 	@Override
 	public Role saveRole(Role role) {
+		log.info("Saving new role {} to the database", role.getName());
 		return roleRepo.save(role);
 	}
 
 	@Override
 	public void addRoleToUser(String username, String roleName) {
+		log.info("Adding role {} to user {}", roleName, username);
 		User user = userRepo.findByUserName(username);
 		Role role = roleRepo.findByName(roleName);
 		user.getRoles().add(role);
@@ -41,11 +44,13 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public User getUser(String username) {
+		log.info("Fetching user {}", username);
 		return userRepo.findByUserName(username);
 	}
 
 	@Override
 	public List<User> getUsers() {
+		log.info("Fetching all users");
 		return userRepo.findAll();
 	}
 
