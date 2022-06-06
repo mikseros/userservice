eea9eaae07c9920363f69c49ce55372f78c7a593
mikseros <mmadynski@gmail.com>
2022-06-06 22:36:28 +0200

    Refresh Token

--- src/main/java/com/mikseros/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/mikseros/userservice/filter/CustomAuthenticationFilter.java
@@ -53,6 +53,11 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 				.withIssuer(request.getRequestURL().toString())
 				.withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
 				.sign(algorithm);
+		String refresh_token = JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+				.withIssuer(request.getRequestURL().toString())
+				.sign(algorithm);
 	}
 	
 	
