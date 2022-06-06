75724ac5144bf128d852f8f1f1bb97bca38cc70d
mikseros <mmadynski@gmail.com>
2022-06-06 22:34:06 +0200

    Access Token

--- src/main/java/com/mikseros/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/mikseros/userservice/filter/CustomAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package com.mikseros.userservice.filter;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -11,7 +14,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,9 +44,15 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
-			Authentication authResult) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		super.successfulAuthentication(request, response, chain, authResult);
+			Authentication authentication) throws IOException, ServletException {
+		User user = (User)authentication.getPrincipal();
+		Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+		String access_token = JWT.create()
+				.withSubject(user.getUsername())
+				.withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
+				.withIssuer(request.getRequestURL().toString())
+				.withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+				.sign(algorithm);
 	}
 	
 	
