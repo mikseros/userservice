55e9c964f66e2a714bb19b2de16d4e366fe4fad6
mikseros <mmadynski@gmail.com>
2022-06-07 11:23:45 +0200

    Sending User Tokens (JSON) to the output.

--- src/main/java/com/mikseros/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/mikseros/userservice/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.mikseros.userservice.filter;
 
 import java.io.IOException;
 import java.sql.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -10,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -58,10 +62,14 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 				.withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
 				.withIssuer(request.getRequestURL().toString())
 				.sign(algorithm);
-		response.setHeader("access_token", access_token);
-		response.setHeader("refresh_token", refresh_token);
+//		response.setHeader("access_token", access_token);
+//		response.setHeader("refresh_token", refresh_token);
+		Map<String, String> tokens = new HashMap<>();
+		tokens.put("access_token", access_token);
+		tokens.put("refresh_token", refresh_token);
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		new ObjectMapper().writeValue(response.getOutputStream(), tokens);
 	}
-	
 	
 	
 }
